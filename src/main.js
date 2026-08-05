9e36632ea69e09052a24116c724cf47db4ceec02
dashaOdinDvaTri <dashaOdinDvaTri@users.noreply.github.com>
2026-08-05 23:41:07 +0300

    Add animated video intro

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import "./styles/main.css";
 import { STORE_LINKS } from "./scripts/config.js";
 import { initFaq } from "./scripts/faq.js";
+import { initIntro } from "./scripts/intro.js";
 import {
   initRevealAnimations,
   initSmoothAnchorScroll,
 } from "./scripts/motion.js";
 import { initStoreLinks } from "./scripts/store-links.js";
 
+initIntro();
 initStoreLinks(STORE_LINKS);
 initFaq();
 initRevealAnimations();
